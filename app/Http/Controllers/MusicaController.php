421fc9ba156951f6e0d0758d540f7cd6fda342a3
gusrapaiva <gustavorapaiva@gmail.com>
2024-05-16 14:12:42 -0300

    remocao de erro

--- app/Http/Controllers/MusicaController.php
+++ app/Http/Controllers/MusicaController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Models\musica;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class MusicaController extends Controller
 {
@@ -12,7 +15,10 @@ class MusicaController extends Controller
      */
     public function index()
     {
-        //
+        $dados = musica::all();
+        $counter = $dados->count();
+
+        return "Músicas: " . $counter . $dados . Response()->json([], Response::HTTP_NO_CONTENT);
     }
 
     /**
@@ -20,30 +26,79 @@ class MusicaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $dados = $request->all();
+
+        $validarDados = Validator::make($dados, [
+            "nome" => 'required',
+            "tempo" => 'required',
+            "id_artista" => 'required',
+            "id_album" => 'required',
+        ]);
+
+        if($validarDados->fails()){
+            return "Dados inválidos" . $validarDados->error(true) . 500;
+        }
+
+        $dadosCadastrar = Musica::create($dados);
+        if($dadosCadastrar){
+            return "Dados cadastrados com sucesso." . Response()->json([], Response::HTTP_NO_CONTENT);
+        } else {
+            return "Falha ao cadastrar dados." . Response()->json([], Response::HTTP_NO_CONTENT);
+        }
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(musica $musica)
+    public function show(string $id)
     {
-        //
+        $dados = Musica::find($id);
+
+        if($dados){
+            return "Música encontrada com sucesso." . Response()->json([], Response::HTTP_NO_CONTENT);
+        } else {
+            return "Música não existe." . Response()->json([], Response::HTTP_NO_CONTENT);
+        }
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, musica $musica)
+    public function update(Request $request, string $id)
     {
-        //
+        $musicas = $request->all();
+
+        $validarDados = Validator::make($musicas, [
+            'nome' => 'required',
+            'tempo' => 'required',
+            'id_artista' => 'required',
+            'id_album' => 'required',
+        ]);
+
+        if($validarDados->fails()){
+            return "Dados inválidos." . $validarDados->error(true) . 500;
+        }
+
+        $musica = Musica::find($id);
+        $musica->nome = $musicas['nome'];
+        $musica->tempo = $musicas['tempo'];
+        $musica->id_artista = $musicas['id_artista'];
+        $musica->id_album = $musicas['id_album'];
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(musica $musica)
+    public function destroy(string $id)
     {
-        //
+        $musica = musica::find($id);
+
+        if($musica){
+            return "Música encontrada com sucesso." . Response()->json([], Response::HTTP_NO_CONTENT);
+        } else {
+            return "Falha ao encontrar música." . Response()->json([], Response::HTTP_NO_CONTENT);
+
+        }
     }
 }
